e73fefc9e17cb0568fcd7f719d58c921a4d3a261
Oussama Benramdane <oussama.benramdane@azimut.net>
2021-07-01 16:24:51 +0200

    experimenting

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,6 +6,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+
+
+
 class ArticleController extends Controller {
 
     /**
